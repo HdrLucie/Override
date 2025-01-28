0f1efa2da552d45cb966703425bf200a108bd50c
ygaude <ygaude@student.42.fr>
2025-01-28 13:17:27 +0100

    Cleanup level02 source

--- level02/source.c
+++ level02/source.c
@@ -2,52 +2,47 @@
 #include <string.h>
 #include <stdlib.h>
 
-undefined8 main(void) {
-    char local_118[112] = {0};
-    char local_a8[48] = {0};
-    char local_78[100] = {0};
-    int local_14 = 0;
-    FILE *local_10 = NULL;
-    size_t sVar2;
+int main(void) {
+    char input_pass[112] = {0};
+    char pass[48] = {0};
+    char username[100] = {0};
+    size_t len = 0;
+    FILE *passfile = NULL;
 
-    local_10 = fopen("/home/users/level03/.pass", "r");
-    if (local_10 == NULL) {
-        fwrite("ERROR: failed to open password file\n", 1, 0x24, stderr);
+    passfile = fopen("/home/users/level03/.pass", "r");
+    if (passfile == NULL) {
+        fwrite("ERROR: failed to open password file\n", 1, 36, stderr);
         exit(1);
     }
-    sVar2 = fread(local_a8, 1, 0x29, local_10);
-    local_14 = (int)sVar2;
-    sVar2 = strcspn(local_a8, "\n");
-    local_a8[sVar2] = '\0';
+    len = fread(pass, 1, 41, passfile);
+    pass[strcspn(pass, "\n")] = '\0';
 
-    if (local_14 != 0x29) {
-        fwrite("ERROR: failed to read password file\n", 1, 0x24, stderr);
-        fwrite("ERROR: failed to read password file\n", 1, 0x24, stderr);
+    if (len != 41) {
+        fwrite("ERROR: failed to read password file\n", 1, 36, stderr);
+        fwrite("ERROR: failed to read password file\n", 1, 36, stderr);
         exit(1);
     }
-    fclose(local_10);
+    fclose(passfile);
 
     puts("===== [ Secure Access System v1.0 ] =====");
     puts("/***************************************\\");
     puts("| You must login to access this system. |");
     puts("\\**************************************/");
-    
+
     printf("--[ Username: ");
-    fgets(local_78, 100, stdin);
-    sVar2 = strcspn(local_78, "\n");
-    local_78[sVar2] = '\0';
+    fgets(username, 100, stdin);
+    username[strcspn(username, "\n")] = '\0';
 
     printf("--[ Password: ");
-    fgets(local_118, 100, stdin);
-    sVar2 = strcspn(local_118, "\n");
-    local_118[sVar2] = '\0';
+    fgets(input_pass, 100, stdin);
+    input_pass[strcspn(input_pass, "\n")] = '\0';
     puts("*****************************************");
-    if (strncmp(local_a8, local_118, 0x29) == 0) {
-        printf("Greetings, %s!\n", local_78);
+    if (strncmp(pass, input_pass, 41) == 0) {
+        printf("Greetings, %s!\n", username);
         system("/bin/sh");
         return 0;
     }
-    printf(local_78);
+    printf(username);
     puts(" does not have access!");
     exit(1);
 }
